ff7b0d1d3d0d1e8d8350b6d60424c867907c31fd
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-05-12 17:06:00 +0430

    feat: create surveys.module and add it to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SurveysModule } from './surveys/surveys.module';
 
 @Module({
-  imports: [],
+  imports: [SurveysModule],
   controllers: [AppController],
   providers: [AppService],
 })
