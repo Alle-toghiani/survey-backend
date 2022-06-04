0a603ceb6898f972940cf758cefdba5f33c7d428
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-06-04 19:58:00 +0430

    feat(survey.module): import TypeOrmModule.forFeature

--- src/surveys/surveys.module.ts
+++ src/surveys/surveys.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { SurveyController } from './controllers/survey.controller';
 import { SurveyService } from './services/survey.service';
 import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Survey } from './survey.entity';
+
+
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, TypeOrmModule.forFeature([Survey])],
   controllers: [SurveyController],
   providers: [SurveyService]
 })
