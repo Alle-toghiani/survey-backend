03e0365e22c79c30c6993dbac189e8bcf49c61c0
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-06-07 14:29:45 +0430

    feat(entities): create survey-question.entity

---
+++ src/surveys/entities/survey-question.entity.ts
@@ -0,0 +1,50 @@
+import { Column } from "typeorm";
+import { SurveyCharts } from "./survey-charts.entity";
+import { SurveyChoice } from "./survey-choices.entity";
+
+export class SurveyQuestion{
+    @Column(type => SurveyChoice)
+    choices: SurveyChoice[];
+
+    @Column(type => SurveyCharts)
+    charts: SurveyCharts;
+
+    @Column()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    type: number;
+
+    @Column()
+    order: number;
+
+    @Column()
+    show_charts: boolean;
+
+    @Column()
+    image_video_active: boolean;
+
+    @Column()
+    image_or_video: number;
+
+    @Column()
+    image_path: string;
+
+    @Column()
+    video_url: string;
+
+    @Column()
+    image_name: string; 
+
+    @Column()
+    question_number_is_hidden: boolean;
+
+    @Column()
+    related_group: string;
+
+    @Column()
+    allow_multiple_select: boolean;
+}
