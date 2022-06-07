27ddcfa12e10d1c7f393293860d67a873ae119d8
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-06-07 14:28:59 +0430

    feat(entities): create survey-charts.entity

---
+++ src/surveys/entities/survey-charts.entity.ts
@@ -0,0 +1,18 @@
+import { Column } from "typeorm";
+
+export class SurveyCharts{
+    @Column()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    type: number | string;
+
+    @Column()
+    order: number;
+
+    @Column()
+    caption: string;
+}
