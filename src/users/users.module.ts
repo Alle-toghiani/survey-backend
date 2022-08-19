1080d98f8eefe06a44754223a807af8894682009
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-19 15:01:51 +0430

    feat(users.module): import HttpModule, and add AuthService to providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,15 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { User } from './entities/user.entity';
+import { AuthService } from './services/auth.service';
 import { UsersService } from './services/users.service';
 import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [HttpModule, TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, AuthService]
 })
 export class UsersModule {}
