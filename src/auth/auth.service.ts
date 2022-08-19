9522c2c96ecc9aa53dc036d522a842a5f71269dd
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-19 17:39:29 +0430

    feat(auth/): create auth.service.ts

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,72 @@
+import {
+    Injectable,
+    BadRequestException,
+    NotFoundException,
+  } from '@nestjs/common';
+  import { JwtService } from '@nestjs/jwt';
+
+  import { promisify } from 'util';
+  import { randomBytes, scrypt as _scrypt } from 'crypto';
+
+import { UsersService } from 'src/users/services/users.service';
+  
+  const scrypt = promisify(_scrypt);
+  
+  @Injectable()
+  export class AuthService {
+
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+        ) {}
+  
+    async signup(email: string, password: string) {
+      // See if email is in usea
+      const users = await this.usersService.find(email);
+      if (users.length) {
+        throw new BadRequestException('email in use');
+      }
+  
+      // Hash the users password
+      // Generate a salt
+      const salt = randomBytes(8).toString('hex');
+  
+      // Hash the salt and the password together
+      const hash = (await scrypt(password, salt, 32)) as Buffer;
+  
+      // Join the hashed result and the salt together
+      const result = salt + '.' + hash.toString('hex');
+  
+      // Create a new user and save it
+      const user = await this.usersService.create(email, result);
+  
+      // return the user
+      return user;
+    }
+  
+    async signin(email: string, password: string) {
+      const [user] = await this.usersService.find(email);
+      if (!user) {
+        throw new NotFoundException('user not found');
+      }
+  
+      const [salt, storedHash] = user.password.split('.');
+  
+      const hash = (await scrypt(password, salt, 32)) as Buffer;
+  
+      if (storedHash !== hash.toString('hex')) {
+        throw new BadRequestException('bad password');
+      }
+
+      let payload = { 
+        username: user.username,
+        role: user.parentId ? 'ADMIN' : 'MOD',
+        hasToken: !user.parentId && user.apiToken
+        };
+
+      return {
+      access_token: this.jwtService.sign(payload),
+    };
+    }
+  }
+  
