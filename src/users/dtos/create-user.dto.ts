4abb8bdfb53e145c6449546a34984dcab1d61381
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-19 12:42:15 +0430

    feat(users/dtos): create create-user.dto

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsString } from '@nestjs/class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
