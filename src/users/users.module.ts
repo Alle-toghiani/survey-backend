d2716e53f9d8fce9416aaba4fa4b6332f5b4c9c8
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-25 11:52:21 +0430

    feat(users.module): imoprt HttpModule, add UsersHttpService to Providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './entities/user.entity';
+import { UsersHttpService } from './services/users-http.service';
 import { UsersService } from './services/users.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    HttpModule
   ],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    UsersHttpService
+  ],
   exports: [UsersService]
 })
 export class UsersModule {}
