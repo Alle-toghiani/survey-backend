133050e065d49029b47e3165ff7870a148f35e14
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-25 11:49:39 +0430

    feat(users/users.service): add setApiToken and isTokenValid methods

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,14 +1,18 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Repository } from 'typeorm';
 
 import { User } from '../entities/user.entity';
+import { UsersHttpService } from './users-http.service';
 
 @Injectable()
 export class UsersService {
 
-  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private repo: Repository<User>,
+    private usersHttpService: UsersHttpService
+    ) {}
 
   create(username: string, email: string, password: string) {
       const user = this.repo.create({ username, email, password });
@@ -43,5 +47,36 @@ export class UsersService {
       throw new NotFoundException('user not found');
     }
     return this.repo.remove(user);
+  }
+
+  async setApiToken(username: string, apiToken: string){
+    const user = await this.findOne(username);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    if (user.parentId) {
+      throw new UnauthorizedException('only admins can set api token')
+    }
+
+    const isTokenValid = await this.isTokenValid(apiToken);
+    if (!isTokenValid) {
+      throw new UnauthorizedException('Invalid token');
+    } else {
+      const updatedUser = {...user, apiToken: apiToken};
+      return this.update(user.username, updatedUser);
+    }
+  }
+
+  async isTokenValid(apiToken: string){
+    return new Promise(
+      resolve => {
+        this.usersHttpService.getFolders(apiToken).subscribe(
+          {
+            next: (getFolderRes) => resolve(getFolderRes.status === HttpStatus.OK),
+            error: () => resolve(false)
+          }
+        )
+      }
+    )
   }
 }
