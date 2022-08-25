02f9f587ac762027355c33b197f0ef6d6877f6c5
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-25 17:56:57 +0430

    feat(surveys.module): add SurveysHttpService to providers array

--- src/surveys/surveys.module.ts
+++ src/surveys/surveys.module.ts
@@ -4,11 +4,15 @@ import { SurveyService } from './services/survey.service';
 import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Survey } from './entities/survey.entity';
+import { SurveysHttpService } from './services/surveys-http.service';
 
 
 @Module({
   imports: [HttpModule, TypeOrmModule.forFeature([Survey])],
   controllers: [SurveyController],
-  providers: [SurveyService]
+  providers: [
+    SurveyService,
+    SurveysHttpService
+  ]
 })
 export class SurveysModule {}
