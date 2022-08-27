1789a3d1054f3dc01d6bf51a66e16d5a25507faf
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 12:03:28 +0430

    feat(surveys-http.service): create getQuestionsList, getQuestion, getSurvey

--- src/surveys/services/surveys-http.service.ts
+++ src/surveys/services/surveys-http.service.ts
@@ -3,6 +3,7 @@ import { HttpService } from '@nestjs/axios';
 import { firstValueFrom, map, Observable } from 'rxjs';
 import { FolderModel } from '../models/folder.model';
 import { environment } from 'src/environments/environment';
+import { Survey } from '../entities/survey.entity';
 @Injectable()
 export class SurveysHttpService {
     constructor(
@@ -27,8 +28,58 @@ export class SurveysHttpService {
         )
       }
 
+  // get List of Surveys
   async getFolders(): Promise<any>{
     const url = environment.baseApiUrl + 'folders/';
+
     return await firstValueFrom(this.http.get(url, {params : {nested: true}}).pipe(map(item => item.data)));
-  }  
+  }
+
+  // get List of Questions
+  async getSurvey(surveyId: number): Promise<any>{
+    const url = environment.baseApiUrl + 'surveys/'+ surveyId + '/detailedQuestions2/';
+
+    return await firstValueFrom(this.http.get(url).pipe(map(item => ({...item.data, questions: item.data.questions.sort( (a,b) => a.order - b.order )}))));
+  }
+
+  // get List of Choices
+  async getQuestion(surveydId:string, questionId: string): Promise<any>{
+    return new Promise(
+      (resolve, reject) => {
+        const url = environment.baseApiUrl + 'surveys/' + surveydId + '/charts/from/2000-10-10/to/2099-10-10/';
+        this.http.get(url).subscribe(
+          {
+            next: (questionsRes) => {
+                if (questionsRes.status === HttpStatus.OK){
+                  if (Object.values(questionsRes.data.data).length > 0){
+                    const question = Object.values(questionsRes.data.data).find( (item: any) => item.id === +questionId);
+                    if (question) resolve(question);
+                    else reject("Question not found")
+                  } 
+                }
+            },
+            error: (error) => reject("Question not found")
+          }
+        )
+      }
+    )
+  }
+  
+  async getQuestionsList(surveydId:string): Promise<any>{
+    return new Promise(
+      (resolve, reject) => {
+        const url = environment.baseApiUrl + 'surveys/' + surveydId + '/charts/from/2000-10-10/to/2099-10-10/';
+        this.http.get(url).subscribe(
+          {
+            next: (questionsRes) => {
+                if (questionsRes.status === HttpStatus.OK){
+                  resolve(Object.values(questionsRes.data.data))
+                }
+            },
+            error: (error) => reject("Question List not found")
+          }
+        )
+      }
+    )
+  }
 }
