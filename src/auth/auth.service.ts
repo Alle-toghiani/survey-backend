bcfb056b8db3fc5f408b0648cea6f0afb0709df2
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-27 20:05:55 +0430

    feat(auth.service): add createMod method, deleteMod getModsList

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,16 +2,19 @@ import {
     Injectable,
     BadRequestException,
     NotFoundException,
+    HttpStatus,
   } from '@nestjs/common';
   import { JwtService } from '@nestjs/jwt';
 
   import { promisify } from 'util';
   import { randomBytes, scrypt as _scrypt } from 'crypto';
 
+  import { AccessTokenModel } from './models/access-token.model';
+  
 import { UsersService } from 'src/users/services/users.service';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
-import { AccessTokenModel } from './models/access-token.model';
 import { User } from 'src/users/entities/user.entity';
+import { CreateModDto } from 'src/users/dtos/create-mod.dto';
   
   const scrypt = promisify(_scrypt);
   
@@ -91,5 +94,58 @@ import { User } from 'src/users/entities/user.entity';
       return this.getAccessToken(updateUserApiTokenRes);
     }
 
+    async createMod(admin: User, modUserName: string) {
+      // See if email is in usea
+      const users = await this.usersService.find(modUserName);
+      if (users.length) {
+        throw new BadRequestException('username in use');
+      }
+
+      // Hash the users password
+      // Generate a salt
+      const salt = randomBytes(8).toString('hex');
+
+      const generatedPassword = this._generateRandomPassword();
+  
+      // Hash the salt and the password together
+      const hash = (await scrypt(generatedPassword, salt, 32)) as Buffer;
+  
+      // Join the hashed result and the salt together
+      const hashedSaltedPassword = salt + '.' + hash.toString('hex');
+
+      const modData: CreateModDto = {
+        username: modUserName,
+        password: hashedSaltedPassword,
+        parentId: admin.username,
+      }
+  
+      // Create a new user and save it
+      const user = await this.usersService.createMod(modData);
+  
+      // return the user
+      if (user){
+        return {
+          username: user.username,
+          password: generatedPassword
+        }
+      }
+    }
+
+    private _generateRandomPassword(): string{
+      var pwdChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+      var pwdLen = 10;
+      return Array(pwdLen).fill(pwdChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+    }
+
+    async getModsList(user: User){
+      return this.usersService.getModsList(user.username);
+    }
+
+    async deleteMod(user: User, modUserName){
+      const modData = await this.usersService.findOne(modUserName);
+      if (modData.parentId === user.username){
+        return this.usersService.remove(modUserName);
+      }
+    }
   }
   
