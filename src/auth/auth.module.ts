643009d6c88c7d24d92091e1dfd663398faff01b
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-28 10:55:57 +0430

    refactor(auth.module.ts): increase jwt token expire time from 1h to 6h

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -20,7 +20,7 @@ import { jwtConstants } from './constants';
         PassportModule,
         JwtModule.register({
         secret: jwtConstants.secret,
-        signOptions: { expiresIn: '60m' },
+        signOptions: { expiresIn: '360m' },
         })
     ],
     providers: [AuthService, JwtStrategy],
