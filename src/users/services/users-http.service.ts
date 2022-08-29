99804e88060ea453a459c0d089f1eabcb24f3a4c
Ali Toghiani <ali.toghiani.dev@gmail.com>
2022-08-29 19:41:56 +0430

    fix(users-http.service)

--- src/users/services/users-http.service.ts
+++ src/users/services/users-http.service.ts
@@ -1,5 +1,6 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom, map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 import { FolderModel } from 'src/surveys/models/folder.model';
@@ -10,9 +11,9 @@ export class UsersHttpService {
         private http: HttpService
     ){}
 
-    getFolders(apiToken: string){
+    getFolders(apiToken: string): any{
         const url = environment.baseApiUrl + 'folders';
         const header = {'Authorization' : apiToken };
-        return this.http.get<FolderModel[]>(url, {headers: header});
+        return firstValueFrom(this.http.get<FolderModel[]>(url, {headers: header}));
     }
 }
